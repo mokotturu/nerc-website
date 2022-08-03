e7dffe8f4774c071c79025a354ff15e0c5bce0b7
mokotturu <monishkotturu@gmail.com>
2022-08-03 14:09:03 -0400

    landing page changes and new speaker

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
 		extend: {
 			colors: {
 				'umlblue': {
+					100: '#EEF2FC',
+					200: '#CBD9F6',
 					500: '#3068D9',
-					900: 'rgba(16, 37, 64)'
+					900: '#102540',
 				},
 				'umlbluedarktransparent': 'rgba(16, 37, 64, 0.7)',
 			},
