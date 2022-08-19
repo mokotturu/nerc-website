fde82db70e47a8652e8469d8e9de99b38dfd88a3
mokotturu <monishkotturu@gmail.com>
2022-08-18 21:17:32 -0400

    added hotels nearby

--- map.js
+++ map.js
@@ -2,10 +2,34 @@
  * Open Street Maps using Leaflet https://leafletjs.com/
  */
 
-let map = L.map('map').setView([42.64369, -71.30703], 13);
+// event location
+let map0 = L.map('map0').setView([42.64369, -71.30703], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-L.marker([42.64369, -71.30703]).addTo(map).bindPopup('UMass Lowell Inn & Conference Center');
-L.marker([42.64192295932301, -71.31426226292896]).addTo(map).bindPopup('NERVE Center');
+	attribution:
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map0);
+L.marker([42.64369, -71.30703])
+	.addTo(map0)
+	.bindPopup('UMass Lowell Inn & Conference Center');
+L.marker([42.64192295932301, -71.31426226292896])
+	.addTo(map0)
+	.bindPopup('NERVE Center');
+
+// hotels nearby
+let map1 = L.map('map1').setView([42.64369, -71.30703], 12);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution:
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map1);
+L.marker([42.64369, -71.30703])
+	.addTo(map1)
+	.bindPopup('UMass Lowell Inn & Conference Center');
+L.marker([42.61521, -71.31746]).addTo(map1).bindPopup('Sonesta Select');
+L.marker([42.603662676037835, -71.34761911813817])
+	.addTo(map1)
+	.bindPopup('Holiday Inn Express');
+L.marker([42.6419511985783, -71.24046438304549])
+	.addTo(map1)
+	.bindPopup('Fairfield Inn by Marriot');
